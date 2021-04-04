01f5ab0a93a30e0bb2c6d071948e1068ca6b55a3
Carlo Conserva <carlo.conserva@gmail.com>
2021-04-04 15:53:36 +0100

    Sort implementation

--- src/Lib.hs
+++ src/Lib.hs
@@ -6,8 +6,35 @@ someFunc = (fmap sortAlphabetically source) >>= printList
 source :: IO [String]
 source = pure ["one", "two", "three"]
 
+data CompareResult = Preceding | Equal | Following
+  deriving (Eq)
+
+newtype Comparator a = Comparator (a -> a -> CompareResult)
+
+minim :: Comparator a -> a -> a -> a
+minim (Comparator f) a b
+    | f a b == Preceding = a
+    | otherwise = b
+      
+minimumInList :: Comparator a -> [a] -> (a, Int)
+minimumInList (Comparator f) list = let comp = Comparator (\x y -> f (fst x) (fst y)) in 
+                                    foldl1 (minim comp) (zip list [0..]) 
+
+sortList :: Comparator a -> [a] -> [a]
+sortList _ [] = []
+sortList comp list = let m = minimumInList comp list in 
+                     (fst m : sortList comp (let (l1,l2) = splitAt (snd m) list in 
+                                             l1 ++ tail l2)
+                     )
+
+compareStrings :: String -> String -> CompareResult
+compareStrings s1 s2  
+  | s1 < s2 = Preceding
+  | s1 == s2 = Equal
+  | otherwise = Following
+
 sortAlphabetically :: [String] -> [String]
-sortAlphabetically a = a
+sortAlphabetically = sortList (Comparator compareStrings)
 
 printList :: [String] -> IO ()
 printList list = fmap (const ()) (traverse putStrLn list)
